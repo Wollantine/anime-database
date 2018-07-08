6e850d4b3f7c6ee9932b48ec24126e271dbed923
Kwirke <nebrios2@gmail.com>
2018-07-08 22:35:58 +0200

    Prepare build test

--- src/components/AnimeTable/Pagination/redux/PaginationActions.ts
+++
@@ -1,6 +0,0 @@
-export const CHANGE_PAGE = 'CHANGE_PAGE';
-
-export const changePage = (page: number) => ({
-    type: CHANGE_PAGE,
-    page,
-});
--- src/components/AnimeTable/Pagination/redux/PaginationState.ts
+++
@@ -1,4 +0,0 @@
-export interface IPaginationState {
-    page: number;
-    pageSize: number;
-}
